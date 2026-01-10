5fc74adb2a07ea7e34a5847994d4801f1bdf399d
CorradoColaleo <148058216+CorradoColaleo@users.noreply.github.com>
2026-01-10 10:50:05 +0100

    general update

--- Generazione/script_ausiliari/script_per_togliere_le_entry_dal_json_già_processate.py
+++ Generazione/script_ausiliari/script_per_togliere_le_entry_dal_json_già_processate.py
@@ -1,7 +1,7 @@
 import json
 
 # Carica il JSON di input da file
-with open("", "r", encoding="utf-8") as f:
+with open("C:\\Users\\corra\\Desktop\\università\\AISE\\progetto\\Argus\\Generazione\\script_ausiliari\\dataset_generazione_parziale_2_with_prompts.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 # Prima operazione: sostituisce output vuoto con 1
@@ -9,11 +9,16 @@ for entry in data:
     if entry.get("output", None) == "":
         entry["output"] = 1
 
-# Seconda operazione: filtra solo le entry con output == 1
-filtered_data = [entry for entry in data if entry.get("output") == 1]
+# Salva tutte le entry con output != 1
+output_not_ones = [entry for entry in data if entry.get("output") != 1]
 
-# Salva il nuovo JSON filtrato
+with open("output_processed.json", "w", encoding="utf-8") as f:
+    json.dump(output_not_ones, f, indent=4, ensure_ascii=False)
+
+# Salva solo le entry con output == 1
+output_only_ones = [entry for entry in data if entry.get("output") == 1]
+
 with open("output_only_ones.json", "w", encoding="utf-8") as f:
-    json.dump(filtered_data, f, indent=4, ensure_ascii=False)
+    json.dump(output_only_ones, f, indent=4, ensure_ascii=False)
 
 print("Operazione completata.")
